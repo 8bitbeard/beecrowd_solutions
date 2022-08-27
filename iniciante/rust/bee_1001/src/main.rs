3fc6e61b0cbd7a598f56d286abc0576d53d6e8c9
Wilton Souza <willsouzafilho@gmail.com>
2022-08-27 18:03:02 -0300

    Adding the problem 1001 solution

---
+++ iniciante/rust/bee_1001/src/main.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+fn main() {
+    let mut a = String::new();
+    let mut b = String::new();
+
+    io::stdin().read_line(&mut a).expect("Failed to read line!");
+
+    let a: i32 = a.trim().parse::<i32>().unwrap();
+
+    io::stdin().read_line(&mut b).expect("Failed to read line!");
+
+    let b: i32 = b.trim().parse::<i32>().unwrap();
+
+    let x: i32 = a + b;
+
+    println!("X = {}", x);
+}
