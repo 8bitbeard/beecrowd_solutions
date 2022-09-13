5a52e9682e1dbc13b1b16a12c53b9815f72a6970
Wilton Souza <willsouzafilho@gmail.com>
2022-09-13 18:03:02 -0300

    Adding the solution for the problem 1064

---
+++ iniciante/rust/bee_1064/src/main.rs
@@ -0,0 +1,28 @@
+use std::io;
+
+fn main() {
+
+    let mut v: Vec<f64> = Vec::new();
+    let mut counter: u8 = 0;
+
+    while counter < 6 {
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let input: f64 = input.trim().parse().unwrap();
+
+        v.push(input);
+
+        counter += 1;
+    };
+
+    v.retain(|&i| i > 0.0);
+
+    let media: f64 = v.iter().sum::<f64>() / (v.len() as f64);
+
+    println!("{} valores positivos", v.len());
+    println!("{:.1}", media);
+}
