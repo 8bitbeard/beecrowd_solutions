5edb5bb4ffe1f824665b8364c0642d03746f41d8
Wilton Souza <willsouzafilho@gmail.com>
2022-10-06 18:03:02 -0300

    Adding the solution for the problem 1164

---
+++ iniciante/rust/bee_1164/src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+
+fn main() {
+    let mut test_cases = String::new();
+
+    io::stdin()
+        .read_line(&mut test_cases)
+        .expect("Failed to read line");
+
+    let test_cases: i32 = test_cases.trim().parse().unwrap();
+
+    for _i in 1..=test_cases {
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let input: i32 = input.trim().parse().unwrap();
+
+        let mut divisors: Vec<i32> = Vec::new();
+
+        for i in 1..=input / 2 {
+            if input % i == 0 {
+                divisors.push(i);
+            }
+        }
+
+        if divisors.iter().sum::<i32>() == input {
+            println!("{} eh perfeito", input)
+        } else {
+            println!("{} nao eh perfeito", input);
+        }
+    }
+}
