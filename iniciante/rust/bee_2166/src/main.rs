f426babca2320b8bf477d4452a3e240373fd967a
Wilton Souza <willsouzafilho@gmail.com>
2022-11-02 10:55:15 -0300

    Adding the solution for the problem 2166

---
+++ iniciante/rust/bee_2166/src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() {
+    let mut number = String::new();
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read tests");
+    let number: usize = number.trim().parse().unwrap();
+
+    let mut factor: f64 = 0.0;
+    for _i in 0..number {
+        factor = 1.0 / (2.0 + factor)
+    }
+
+    println!("{:.10}", 1.0 + factor);
+}
