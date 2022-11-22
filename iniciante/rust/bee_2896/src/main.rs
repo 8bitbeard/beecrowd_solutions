a0d37f7e39620ad8b5d9ad99e9037195c10f8cc9
Wilton Souza <willsouzafilho@gmail.com>
2022-11-22 06:45:39 -0300

    Adding the solution for the problem 2896

--- iniciante/rust/bee_2896/src/main.rs
+++ iniciante/rust/bee_2896/src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut amount = String::new();
+    io::stdin()
+        .read_line(&mut amount)
+        .expect("Failed to read amount");
+    let amount: usize = amount.trim().parse::<usize>().unwrap();
+
+    for _i in 0..amount {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+        let input: Vec<u32> = input
+            .trim()
+            .split(' ')
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+
+        if input[0] < input[1] {
+            println!("{}", input[0]);
+        } else {
+            println!("{}", input[0] / input[1] + input[0] % input[1]);
+        }
+    }
 }
