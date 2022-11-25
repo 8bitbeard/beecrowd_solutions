6631178872d2907748fa6bc1778950127f813f6f
Wilton Souza <willsouzafilho@gmail.com>
2022-11-25 08:35:01 -0300

    Adding the solution for the problem 3157

---
+++ iniciante/rust/bee_3157/src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read the input");
+
+    println!("Nao se refuta ciencia com opiniao");
+}
