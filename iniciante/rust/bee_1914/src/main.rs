5ea4ef3564e4c49a16a2d0a0d31a23bde2779ead
Wilton Souza <willsouzafilho@gmail.com>
2022-12-05 22:59:21 -0300

    Adding the solution for the problem 1914

--- iniciante/rust/bee_1914/src/main.rs
+++ iniciante/rust/bee_1914/src/main.rs
@@ -1,14 +1,18 @@
-use std::io;
 use std::collections::HashMap;
+use std::io;
 
 fn main() {
     let mut amount = String::new();
-    io::stdin().read_line(&mut amount).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut amount)
+        .expect("Failed to read line");
     let amount: usize = amount.trim().parse().unwrap();
 
     for _i in 0..amount {
         let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
         let input: Vec<&str> = input.trim().split(' ').collect();
 
         let mut game_state = HashMap::new();
@@ -17,11 +21,17 @@ fn main() {
         game_state.insert(input[3], input[2]);
 
         let mut game_total = String::new();
-        io::stdin().read_line(&mut game_total).expect("Failed to read line");
-        let game_total: Vec<u32> = game_total.trim().split(' ').map(|x| x.parse::<u32>().unwrap()).collect();
+        io::stdin()
+            .read_line(&mut game_total)
+            .expect("Failed to read line");
+        let game_total: Vec<u32> = game_total
+            .trim()
+            .split(' ')
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
         let game_total: u32 = game_total.iter().sum::<u32>();
 
-        match game_total {
+        match game_total % 2 {
             0 => println!("{}", game_state.get("PAR").unwrap()),
             _ => println!("{}", game_state.get("IMPAR").unwrap()),
         }
