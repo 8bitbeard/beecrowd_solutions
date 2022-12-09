993e77e8fd77be3afcc41ab5bec905294e9f0b7d
Wilton Souza <willsouzafilho@gmail.com>
2022-12-08 21:38:35 -0300

    Adding the solution for the problem 2761

--- iniciante/rust/bee_2761/src/main.rs
+++ iniciante/rust/bee_2761/src/main.rs
@@ -4,7 +4,25 @@ fn main() {
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
-        .expect("Failed to read line");
+        .expect("Failed to read the line");
     input.pop();
-    let input = input.trim().to_string();
+    let input = input.to_string();
+
+    let (a, b, c, d) = parse_input(input);
+    println!("{}{:.6}{}{}", a, b, c, d);
+    println!("{}\t{:.6}\t{}\t{}", a, b, c, d);
+    println!("{:>10}{:>10.6}{:>10}{:>10}", a, b, c, d);
+}
+
+fn parse_input(input: String) -> (String, f32, String, String) {
+    let idx = input.find(" ").unwrap();
+    let (a, input) = (&input[..idx], &input[idx + 1..]);
+
+    let idx = input.find(" ").unwrap();
+    let (b, input) = (&input[..idx].parse::<f32>().unwrap(), &input[idx + 1..]);
+
+    let idx = input.find(" ").unwrap();
+    let (c, d) = (&input[..idx], &input[idx + 1..]);
+
+    (a.to_string(), *b, c.to_string(), d.to_string())
 }
